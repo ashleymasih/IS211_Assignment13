61444cbb0c1aa862a8fed96d061a8c3354361e9f
ashley <65320362+ashleymasih@users.noreply.github.com>
2023-04-27 00:45:29 -0400

    assignment13

---
+++ recursion.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return (fibonacci(n-1) + fibonacci(n-2))
+    
+def compareTo(s1, s2):
+    if len(s1) == 0 and len(s2) == 0:
+        return 0
+    elif len(s1) == 0:
+        return -1
+    elif len(s2) == 0:
+        return 1
+    else:
+        if s1[0] < s2[0]:
+            return -1
+        elif s1[0] > s2[0]:
+            return 1
+        else:
+            return compareTo(s1[1:], s2[1:])
